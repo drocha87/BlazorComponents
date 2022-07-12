ab012d9cb1d0d7bea9a1ab51ae666afca3fe9ab2
Diego Rocha <dlmrocha87@gmail.com>
2022-07-12 15:53:41 -0300

    introduces Tooltip component

---
+++ src/Drocha.Blazor.Components.Tooltip/Tooltip.razor.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Drocha.Blazor.Components.Tooltip;
+
+public partial class Tooltip : ComponentBase
+{
+
+}
---
+++ src/Drocha.Blazor.Components.Tooltip/wwwroot/tooltip.js
@@ -0,0 +1,5 @@
+"use strict";
+class Tooltip {
+}
+const tooltip = new Tooltip();
+//# sourceMappingURL=tooltip.js.map
