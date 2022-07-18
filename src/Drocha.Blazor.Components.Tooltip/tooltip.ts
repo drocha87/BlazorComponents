8147a996aaf8b6fbe89de85e13dc8ef2024439a1
Diego Rocha <dlmrocha87@gmail.com>
2022-07-18 10:24:44 -0300

    moved popover and tooltip to drblazor project

--- src/Drocha.Blazor.Components.Tooltip/tooltip.ts
+++
@@ -1,8 +0,0 @@
-export function observeMouse(dotnetRef: any, el: HTMLElement) {
-  el.addEventListener("mouseenter", async () =>
-    await dotnetRef.invokeMethodAsync("MouseEnter")
-  );
-  el.addEventListener("mouseleave", async () =>
-    await dotnetRef.invokeMethodAsync("MouseLeave")
-  );
-}
