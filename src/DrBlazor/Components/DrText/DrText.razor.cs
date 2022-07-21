861deea3d1de7f3fa3b476e8fab09588e0bab6bf
Diego Rocha <dlmrocha87@gmail.com>
2022-07-21 12:47:59 -0300

    introduces DrText Component

---
+++ src/DrBlazor/Components/DrText/DrText.razor.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+
+namespace DrBlazor;
+
+public enum TypoStyle
+{
+    [Description("display")] Display,
+    [Description("headline")] Headline,
+    [Description("title")] Title,
+    [Description("label")] Label,
+    [Description("body")] Body,
+}
+
+public enum TypoSize
+{
+    [Description("large")] Large,
+    [Description("medium")] Medium,
+    [Description("small")] Small,
+}
+
+public static class EnumExtension
+{
+    public static string ToDescriptionString<T>(this T @enum) where T : Enum
+    {
+        var fieldInfo = @enum.GetType().GetField(@enum.ToString());
+        if (fieldInfo is not null)
+        {
+            DescriptionAttribute[] attrs = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attrs.Length > 0 ? attrs[0].Description : string.Empty;
+        }
+        return string.Empty;
+    }
+}
+
+public partial class DrText : DrComponentBase
+{
+    [Parameter] public TypoStyle Typo { get; set; } = TypoStyle.Label;
+    [Parameter] public TypoSize Size { get; set; } = TypoSize.Medium;
+
+    [Parameter] public bool GutterBottom { get; set; } = false;
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    private Dictionary<string, object> Attributes =>
+        new AttrBuilder()
+            .AddClass($"{Typo.ToDescriptionString()}-{Size.ToDescriptionString()}")
+            .AddStyle("margin-bottom", "24px", GutterBottom)
+            .Build();
+}
