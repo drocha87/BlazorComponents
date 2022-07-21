5b7b5182f5a9958ddfc49b07281bd0e08839e52d
Diego Rocha <dlmrocha87@gmail.com>
2022-07-21 10:36:01 -0300

    fix DrContainer style

--- src/DrBlazor/Components/DrContainer/DrContainer.razor.cs
+++ src/DrBlazor/Components/DrContainer/DrContainer.razor.cs
@@ -5,17 +5,23 @@ namespace DrBlazor;
 public partial class DrContainer : DrComponentBase
 {
     [Parameter] public RenderFragment ChildContent { get; set; } = null!;
+    [Parameter] public int ElevationLevel { get; set; } = 0;
 
     public Dictionary<string, object> Attributes =>
         new AttrBuilder()
             // css classes
             .AddClass("dr-container")
             .AddClass("shape-medium")
-            .AddClass("surface")
+            .AddClass("primary-container")
+            .AddClass($"dr-elevation-{ElevationLevel}")
             .AddClasses(Class ?? "")
             // styles
             .AddStyle("padding", "24px")
+            // FIXME: it should only be set if the container is fluid
+            .AddStyle("height", "100%")
             .AddStyles(Style ?? "")
+            // data attributes
+            .AddData("id", Id, when: Id is not null)
             // attributes
             .AddAttributes(UserAttributes)
             .Build();
