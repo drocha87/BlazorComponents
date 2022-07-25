a97fd4e014a4bcd45bbc7f4839ec5abb1ebc0a07
Diego Rocha <dlmrocha87@gmail.com>
2022-07-25 17:42:17 -0300

    update DrIcon and DrButtonIcon styles

--- src/DrBlazor/Components/DrButton/DrButtonIcon.razor.cs
+++ src/DrBlazor/Components/DrButton/DrButtonIcon.razor.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace DrBlazor;
 
 public partial class DrButtonIcon : DrComponentBase
 {
     [Parameter] public string Icon { get; set; } = null!;
+    [Parameter] public bool Disabled { get; set; }
 
-    [Parameter] public EventCallback OnClick { get; set; }
+    [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+     private Dictionary<string, object> Attributes =>
+        new AttrBuilder()
+            .AddClass("dr-button-root")
+            .AddClass("dr-button-icon")
+
+            .AddData("disabled", Disabled)
+            .Build();
 }
--- src/DrBlazor/Components/DrIcon/DrIcon.razor.cs
+++ src/DrBlazor/Components/DrIcon/DrIcon.razor.cs
@@ -4,5 +4,13 @@ namespace DrBlazor;
 
 public partial class DrIcon : DrComponentBase
 {
-    [Parameter] public string Icon { get; set; }
+    [Parameter] public string Icon { get; set; } = default!;
+    [Parameter] public int Size { get; set; } = 24;
+
+    private Dictionary<string, object> Attributes =>
+       new AttrBuilder()
+            .AddClass("material-symbols-outlined")
+            .AddClass("dr-icon")
+            .AddStyle("font-size", $"{Size}px")
+            .Build();
 }
